3a654c8be69a8ef5cbe34b185a53240f2a0e3299
Jørgen Busvold <jorgen.busvold@gmail.com>
2019-03-19 15:58:57 +0100

    fix: remove unused file and shuffle code around

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import BookCover from './BookCover';
-import BookShelfChangerMenu from './BookShelfChangerMenu';
 
 class Book extends Component
 {
@@ -13,21 +12,19 @@ class Book extends Component
                 <div className="book-top">
                     <BookCover 
                         coverart={book.coverart} />
-                    {
-                        /* <BookShelfChangerMenu 
-                        book={book} 
-                        onChangeCurrentBookCategory={this.props.onChangeCurrentBookCategory}
-                        /> */
-                    }
                 </div>
 
-                <select value={this.props.book.shelf} onChange={(event) => this.props.onChangeCurrentBookCategory(this.props.book, event)} >
-                    <option value="return" disabled>Move to...</option>
-                    <option value="none">None</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                </select>
+                <div
+                    // className="book-shelf-changer"
+                    >
+                    <select value={this.props.book.shelf} onChange={(event) => this.props.onChangeCurrentBookCategory(this.props.book, event)} >
+                        <option value="return" disabled>Move to...</option>
+                        <option value="none">None</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                    </select>
+                </div>
 
                 <div className="book-title">{book.title}</div>
                 <div className="book-authors">{book.author}</div>
